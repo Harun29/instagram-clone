4dbac7368371ace00c41a38ee87d677ee1e10e4e
Harun29 <117188375+Harun29@users.noreply.github.com>
2023-01-21 15:21:29 +0100

    created post details page and passed id paramater to it using useParams hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './components/dashboard/Home';
+import PostDetails from './components/projects/PostDetails';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/post/:id' element={<PostDetails />}/>
         </Routes>
       </div>
     </Router>
