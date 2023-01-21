d4abf33b794160786726baed4b40637704ba1e4f
Harun29 <117188375+Harun29@users.noreply.github.com>
2023-01-21 14:01:11 +0100

    made basic styling for homepage

--- src/components/projects/PostsList.js
+++ src/components/projects/PostsList.js
@@ -2,38 +2,34 @@ const PostsList = () => {
   return (  
     <div className="posts-list">
 
-      <div className="post-summry">
+      <div className="post-summary">
         <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-Jcpp8c3cW4zOfy2Zwhd_XS-pHGdGxGt4Rp_mTCID&s" alt="" /></div>
-        <div className="username-likes">
-          <h4>Username</h4>
-          <h5>Likes: </h5>
-        </div>
         <div className="description">
+          <h4>Username: </h4>
+          <h5>Likes: </h5>
           <p>About this post</p>
         </div>
       </div>
 
-      <div className="post-summry">
-        <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-Jcpp8c3cW4zOfy2Zwhd_XS-pHGdGxGt4Rp_mTCID&s" alt="" /></div>
-        <div className="username-likes">
-          <h4>Username</h4>
-          <h5>Likes: </h5>
-        </div>
+      <div className="post-summary">
+        <div><img src="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg" alt="" /></div>
         <div className="description">
+          <h4>Username: </h4>
+          <h5>Likes: </h5>
           <p>About this post</p>
         </div>
       </div>
 
-      <div className="post-summry">
+      <div className="post-summary">
         <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-Jcpp8c3cW4zOfy2Zwhd_XS-pHGdGxGt4Rp_mTCID&s" alt="" /></div>
-        <div className="username-likes">
-          <h4>Username</h4>
-          <h5>Likes: </h5>
-        </div>
         <div className="description">
+          <h4>Username: </h4>
+          <h5>Likes: </h5>
           <p>About this post</p>
         </div>
       </div>
+
+      
 
     </div>
   );
