409991cbaec7e7cf4a166200d49fca10cd03a8cb
Harun29 <117188375+Harun29@users.noreply.github.com>
2023-01-22 16:38:28 +0100

    created login form

---
+++ src/components/auth/SignIn.js
@@ -0,0 +1,23 @@
+const SignIn = () => {
+  return ( 
+    <form className="login-form">
+      <label>
+        Email:
+        <input 
+          type="email" 
+        />
+      </label>
+
+      <label>
+        Password:
+        <input 
+          type="password" 
+        />
+      </label>
+
+      <input type="submit" name="" id="" />
+    </form>
+  );
+}
+ 
+export default SignIn;
--- src/components/layout/SignedOutLink.js
+++ src/components/layout/SignedOutLink.js
@@ -1,10 +1,12 @@
 import { NavLink } from "react-router-dom";
+import SignIn from "../auth/SignIn";
 
 const SignedOutLinks = () => {
   return (  
     <ul className="signed-in-out-links">
       <li><NavLink to='/'>Signup</NavLink></li>
       <li><NavLink to='/'>Login</NavLink></li>
+      <SignIn />
     </ul>
   );
 }
