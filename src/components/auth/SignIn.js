d3d56c46f36fc2a648b890944a1df74361a08ff1
harun <ibrahimovic.harun01@gmail.com>
2023-01-27 16:18:51 +0100

    made email and password states

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,16 +1,29 @@
+import { useState } from "react";
+
 const SignIn = ({loginForm, setLoginForm}) => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   return (
     <form className="login-signup-form">
 
       <div className="input-wrapper">
         <label>Email: </label>
-        <input type="email" />
+        <input 
+        type="email"
+        required
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}/>
       </div>
 
       <div className="input-wrapper">
         <label>Password: </label>
-        <input type="password" />
+        <input 
+        type="password" 
+        required
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}/>
       </div>
 
       <input type="submit" name="" id="" value="Login"/>
