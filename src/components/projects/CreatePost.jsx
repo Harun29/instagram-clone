9bb005da6d3127a110bda9fcbfa31ca9e0750c50
Harun29 <117188375+Harun29@users.noreply.github.com>
2023-04-22 19:12:10 +0200

    create post styled

--- src/components/projects/CreatePost.jsx
+++ src/components/projects/CreatePost.jsx
@@ -6,35 +6,75 @@ const CreatePost = () => {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const {addToPosts} = useContext(PostContext);
+  const [photo, setPhoto] = useState(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     addToPosts(title, description);
   };
 
-  return (  
+  const handlePhotoChange = (e) => {
+    if (e.target.files && e.target.files[0]) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        setPhoto(e.target.result);
+      };
+      reader.readAsDataURL(e.target.files[0]);
+    }
+  };
+
+  return (
     <form 
-    className="create-post"
-    onSubmit={handleSubmit}>
-      <h4>Create post</h4>
-      <div className="input-wrapper create-post-inputs">
-        <label>Header</label>
-        <input 
-        type="text" 
-        required
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}/>
+      className="create-post container mb-4"
+      onSubmit={handleSubmit}>
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h4>Create post</h4>
+          <div className="form-group">
+            <label htmlFor="title">Header</label>
+            <input 
+              type="text" 
+              className="form-control"
+              id="title"
+              required
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="description">Description</label>
+            <textarea 
+              id="description"
+              className="form-control"
+              required
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}></textarea>
+          </div>
+          <div className="form-group mb-3">
+            <label htmlFor="photo">Upload photo</label>
+            <div className="input-group">
+              <input 
+                type="file" 
+                className="form-control-file"
+                id="photo"
+                accept="image/*"
+                onChange={handlePhotoChange}/>
+              {photo && (
+                <div className="input-group-prepend">
+                  <div className="photo-preview">
+                    <img src={photo} alt="preview" />
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+          <div className="text-right">
+            <button type="submit" className="btn btn-primary">Post</button>
+          </div>
+        </div>
       </div>
-      <div className="input-wrapper create-post-inputs">
-        <textarea 
-        id="description"
-        required
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}></textarea>
-      </div>
-      <input type="submit" value='post'/>
     </form>
   );
+
 }
 
 export default CreatePost;
