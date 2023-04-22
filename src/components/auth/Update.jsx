ab2abc4917b5df723e9e8f5f2e75a548ef22d859
Harun29 <117188375+Harun29@users.noreply.github.com>
2023-04-22 15:27:25 +0200

    added password update section to settings

--- src/components/auth/Update.jsx
+++ src/components/auth/Update.jsx
@@ -45,53 +45,79 @@ const UpdateProfile = () => {
   //     <div className="loading">Loading...</div>
   //   )
   // }
-
-return user ? (
-  <div className="settings-container d-flex justify-content-center align-items-center shadow p-3 mb-5 bg-white rounded">
-    <div className="d-flex flex-column">
-      <h3 className="mb-3">Settings</h3>
-      <div className="mb-3">
-        <label htmlFor="name" className="form-label">
-          Name
-        </label>
-        <input type="text" className="form-control" id="name" defaultValue={user.name} />
+  
+  return user ? (
+    <div className="settings-container d-flex justify-content-center align-items-start shadow p-3 mb-5 bg-white rounded">
+      <div className="me-5">
+        <h3 className="mb-3">Settings</h3>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input type="text" className="form-control" id="name" defaultValue={user.name} />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="userName" className="form-label">
+            Username
+          </label>
+          <input type="text" className="form-control" id="userName" defaultValue={user.userName} />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email
+          </label>
+          <input type="email" className="form-control" id="email" defaultValue={user.email} />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="bio" className="form-label">
+            Bio
+          </label>
+          <textarea className="form-control" id="bio" rows="3" defaultValue={user.bio}></textarea>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="photo" className="form-label">
+            Profile Photo
+          </label>
+          <input type="file" className="form-control" id="photo" />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Save Changes
+        </button>
       </div>
-      <div className="mb-3">
-        <label htmlFor="userName" className="form-label">
-          Username
-        </label>
-        <input type="text" className="form-control" id="userName" defaultValue={user.userName} />
+      <div>
+        <h4 className="mb-3">Change Password</h4>
+        <div className="mb-3">
+          <label htmlFor="currentPassword" className="form-label">
+            Current Password
+          </label>
+          <input type="password" className="form-control" id="currentPassword" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="newPassword" className="form-label">
+            New Password
+          </label>
+          <input type="password" className="form-control" id="newPassword" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="confirmNewPassword" className="form-label">
+            Confirm New Password
+          </label>
+          <input type="password" className="form-control" id="confirmNewPassword" />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Change Password
+        </button>
       </div>
-      <div className="mb-3">
-        <label htmlFor="email" className="form-label">
-          Email
-        </label>
-        <input type="email" className="form-control" id="email" defaultValue={user.email} />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="bio" className="form-label">
-          Bio
-        </label>
-        <textarea className="form-control" id="bio" rows="3" defaultValue={user.bio}></textarea>
-      </div>
-      <div className="mb-3">
-        <label htmlFor="photo" className="form-label">
-          Profile Photo
-        </label>
-        <input type="file" className="form-control" id="photo" />
-      </div>
-      <button type="submit" className="btn btn-primary">
-        Save Changes
-      </button>
     </div>
-  </div>
-) : (
-  <div className="d-flex justify-content-center align-items-center">
-    <Spinner animation="border" role="status">
-      <span className="visually-hidden">Loading...</span>
-    </Spinner>
-  </div>
-);
+  ) : (
+    <div className="d-flex justify-content-center align-items-center">
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    </div>
+  );
+  
+  
 
 }
  
