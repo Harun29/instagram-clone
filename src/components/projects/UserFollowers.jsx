bf6b847569bfb17452f0ae38fbf79fbfbe5db47e
Harun29 <117188375+Harun29@users.noreply.github.com>
2023-06-04 19:40:26 +0200

    set function to fetch user followers

--- src/components/projects/UserFollowers.jsx
+++ src/components/projects/UserFollowers.jsx
@@ -1,10 +1,34 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useAuth } from "../../context/AuthContext";
 
 const UserFollowers = () => {
 
-  const param = useParams();
+  const { getUserByUsername } = useAuth();
 
-  console.log(param)
+  const param = useParams();
+  const [user, setUser] = useState();
+  const [followers, setFollowers] = useState();
+
+  useEffect(() => {
+    const fetchUserByUsername = async (username) => {
+      const user = await getUserByUsername(username);
+      setUser(user);
+    }
+    try{
+      fetchUserByUsername(param.username)
+    }
+    catch(err){
+      console.error(err)
+    }
+  }, [param, getUserByUsername])
+
+  useEffect(() => {
+    if (user){
+      setFollowers(user.followers)
+    }
+  }, [user])
+
 
   return ( 
     <>
