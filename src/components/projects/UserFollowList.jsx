928f7f899e894a506803ee38c5a10e14dfb2a0b8
Harun29 <117188375+Harun29@users.noreply.github.com>
2023-06-05 22:23:58 +0200

    fixed .map fetching user with an array as username

--- src/components/projects/UserFollowList.jsx
+++ src/components/projects/UserFollowList.jsx
@@ -32,7 +32,6 @@ const UserFollowList = ({ fetchType }) => {
 
 
   useEffect(() => {
-    console.log(fetchType)
     if (user){
       (fetchType === "followers" ? setFollowList(user.followers) : setFollowList(user.following))
     }
@@ -55,7 +54,7 @@ const UserFollowList = ({ fetchType }) => {
   
       try{
         if (followList) {
-          await Promise.all(followList.map((follower) => fetchFollowersPhoto(followList)));
+          await Promise.all(followList.map((follower) => fetchFollowersPhoto(follower)));
           setFollowersWithPictures(followersObject);
         }
       }catch(err){
@@ -68,31 +67,32 @@ const UserFollowList = ({ fetchType }) => {
 
   return (
     <div className="mt-4 container">
-  <h1 className="text-center">User Followers</h1>
-  <div className="row justify-content-center">
-    {followersWithPictures &&
-      Object.keys(followersWithPictures).map((follower) => (
-        <div className="col-lg-3 col-md-4 col-sm-6 mb-4" key={follower}>
-          <div className="card text-center">
-            <div className="card-body">
-              <div className="profile-photo mx-auto">
-                <img
-                  src={followersWithPictures[follower]}
-                  alt="Profile"
-                  className="rounded-circle"
-                  style={{ width: "50px", height: "50px" }}
-                />
+      <h1 className="text-center mb-5">{fetchType === "followers" ? "User Followers" : "User Following"}</h1>
+      
+      <div className="row justify-content-center">
+        {followersWithPictures &&
+          Object.keys(followersWithPictures).map((follower) => (
+            <div className="col-lg-3 col-md-4 col-sm-6 mb-4" key={follower}>
+              <div className="card text-center">
+                <div className="card-body">
+                  <div className="profile-photo mx-auto">
+                    <img
+                      src={followersWithPictures[follower]}
+                      alt="Profile"
+                      className="rounded-circle"
+                      style={{ width: "50px", height: "50px" }}
+                    />
+                  </div>
+                  <h5 className="card-title mt-3">{follower}</h5>
+                  <Link to={`/user/${follower}`} className="btn btn-primary">
+                    View Profile
+                  </Link>
+                </div>
               </div>
-              <h5 className="card-title mt-3">{follower}</h5>
-              <Link to={`/user/${follower}`} className="btn btn-primary">
-                View Profile
-              </Link>
             </div>
-          </div>
-        </div>
-      ))}
-  </div>
-</div>
+          ))}
+      </div>
+    </div>
 
 
   
