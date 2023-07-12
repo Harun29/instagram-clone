6dbe2ad12aaeb9f1a2e9db4821755f7e1ca0e22d
Harun29 <117188375+Harun29@users.noreply.github.com>
2023-07-12 23:06:15 +0200

    test for getting post detail in postList

--- src/components/projects/PostsList.jsx
+++ src/components/projects/PostsList.jsx
@@ -1,4 +1,15 @@
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../../config/firebase";
+
 const PostsList = () => {
+  
+  const docRef = doc(db, 'posts', 'IxToa9xi2mFesn9ds7zY')
+  const docSnap = getDoc(docRef)
+
+  useEffect(() => {
+    console.log(docSnap)
+  }, [docSnap])
+
   return (  
     <>
     </>
