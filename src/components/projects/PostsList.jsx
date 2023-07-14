40e5f5059880f0e34ee2d187eb90c8fd1705ee84
Harun29 <117188375+Harun29@users.noreply.github.com>
2023-07-14 20:06:59 +0200

    created function for getting posts info

--- src/components/projects/PostsList.jsx
+++ src/components/projects/PostsList.jsx
@@ -1,17 +1,29 @@
 import { doc, getDoc } from "firebase/firestore";
+import { useEffect } from "react";
 import { db } from "../../config/firebase";
 
 const PostsList = () => {
 
-  const docRef = doc(db, 'posts', 'IxToa9xi2mFesn9ds7zY');
-  const docSnap = await getDoc(docRef);
+  useEffect(() => {
 
-  if (docSnap.exists()) {
-    console.log("Document data:", docSnap.data());
-  } else {
-    // docSnap.data() will be undefined in this case
-    console.log("No such document!");
-  }
+   const fetchPosts = async (postId) => {
+      const docRef = doc(db, 'posts', postId);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+      } else {
+        // docSnap.data() will be undefined in this case
+        console.log("No such document!");
+      }
+   }
+   
+   try{
+    fetchPosts('IxToa9xi2mFesn9ds7zY')
+   }catch(err){
+    console.error(err)
+   }
+  })
 
   return (  
     <>
