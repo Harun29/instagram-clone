59cee4ffebf3096b34122b40d3582db8acd4d02a
Harun29 <117188375+Harun29@users.noreply.github.com>
2023-09-15 21:38:05 +0200

    post is displayed

--- src/components/projects/Post.jsx
+++ src/components/projects/Post.jsx
@@ -45,7 +45,28 @@ const Post = () => {
   }, [post])
   
 
-  return ( <></> );
+  return (
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-4">
+          {!post ? ( 
+            <p>Loading...</p>
+          ) : (
+            <div className="card">
+              <img src={postPicture} alt="Post Image" className="card-img-top" />
+              <div className="card-body">
+                <h5 className="card-title">{post.title}</h5>
+                <p className="card-text">{post.description}</p>
+                <p className="card-text">
+                  <small className="text-muted">Posted by {post.user}</small>
+                </p>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
  
 export default Post;
