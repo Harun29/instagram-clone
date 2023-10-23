083a38eaba22a425c1bb69bca6ff5a11828817c6
Harun29 <ibrahimovic.harun01@gmail.com>
2023-10-23 21:22:52 +0200

    posts shown on home page

--- src/components/dashboard/Home.jsx
+++ src/components/dashboard/Home.jsx
@@ -14,20 +14,26 @@ useEffect(() => {
     const postsRef = collection(db, "posts");
     const snapshot = await getDocs(postsRef);
 
-    const postsIds = []
-    snapshot.forEach(async(doc) => {
-      const photoUrl = await getDownloadURL(ref(storage, `posts_pictures/${doc.data().photo}`))
-      postsIds.push({
-        id: doc.id,
-        title: doc.data().title,
-        photo: photoUrl,
-        user: doc.data().user
+    const postsData = await Promise.all(
+      snapshot.docs.map(async (doc) => {
+        const photoUrl = await getDownloadURL(
+          ref(storage, `posts_pictures/${doc.data().photo}`)
+        );
+
+        return {
+          id: doc.id,
+          title: doc.data().title,
+          photo: photoUrl,
+          user: doc.data().user,
+        };
       })
-    })
-    setPosts(postsIds)
-  }
-  fetchPosts()
-}, [])
+    );
+
+    setPosts(postsData);
+  };
+
+  fetchPosts();
+}, []);
 
 useEffect(() => {
   console.log(posts)
@@ -40,7 +46,6 @@ return (
       {posts ? posts.map((post) => (
         <div key={post.id} className="col-md-4 mb-4">
           <div className="card">
-            {/* Assuming `photo`, `user`, and `title` are properties of the `data` object */}
             <img
               src={post.photo}
               className="card-img-top"
