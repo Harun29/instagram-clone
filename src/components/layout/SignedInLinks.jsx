fd58e2cd3211bf945c5627b43c90f2404aaec24a
Harun29 <ibrahimovic.harun01@gmail.com>
2023-11-08 20:44:15 +0100

    follow notifications added

--- src/components/layout/SignedInLinks.jsx
+++ src/components/layout/SignedInLinks.jsx
@@ -52,25 +52,42 @@ const SignedInLinks = () => {
   }, [error])
 
   const handleOpened = async(e) => {
-    const notifObject = (notifStatus) => {
+    const notifLikeObject = (notifStatus) => {
       const object = {
         postLiked: e.postLiked,
-          postLikedPhoto: e.postLikedPhoto,
-          likedBy: e.likedBy,
-          likedByPhoto: e.likedByPhoto,
-          opened: notifStatus,
-          notifRef: e.notifRef,
-          notifType: "like"
+        postLikedPhoto: e.postLikedPhoto,
+        likedBy: e.likedBy,
+        likedByPhoto: e.likedByPhoto,
+        opened: notifStatus,
+        notifRef: e.notifRef,
+        notifType: "like"
+      }
+      return object
+    }
+    const notifFollowObject = (notifStatus) => {
+      const object = {
+        followedBy: e.followedBy,
+        likedByPhoto: e.likedByPhoto,
+        opened: notifStatus,
+        notifRef: e.notifRef,
+        notifType: "follow"
       }
       return object
     }
 
-    if(!e.opened){
+    if(!e.opened && e.notifType === "like"){
       await updateDoc(e.notifRef, {
-        notif: arrayRemove(notifObject(false))
+        notif: arrayRemove(notifLikeObject(false))
       });
       await updateDoc(e.notifRef, {
-        notif: arrayUnion(notifObject(true))
+        notif: arrayUnion(notifLikeObject(true))
+      });
+    }else{
+      await updateDoc(e.notifRef, {
+        notif: arrayRemove(notifFollowObject(false))
+      });
+      await updateDoc(e.notifRef, {
+        notif: arrayUnion(notifFollowObject(true))
       });
     }
   }
@@ -101,15 +118,30 @@ const SignedInLinks = () => {
         <ul className="dropdown-menu" style={{ minWidth: '400px', marginLeft: '-225px', marginTop: '10px' }}>
           {notifs ? (
             notifs.map((notif, index) => (
-              <li key={index} className='px-2 list-group-item d-flex align-items-center justify-content-between' id={notif.opened ? 'opened-notification' : 'notification'}>
-                <Link className="me-3 notif-by" to={`/user/${notif.likedBy}`}>
-                  <img src={notif.likedByPhoto ? notif.likedByPhoto : '/blank-profile.jpg'} alt="liked" />
-                  <strong>{notif.likedBy}</strong>
-                </Link>{' '}
-                <Link onClick={() => handleOpened(notif)} className="post-link-notif" to={`/post/${notif.postLiked}`}>
-                  <label>Liked your post</label>
-                  <img src={notif.postLikedPhoto} alt="" />
-                </Link>
+              <li key={index} className='px-2 list-group-item' id={notif.opened ? 'opened-notification' : 'notification'}>
+                {notif.notifType === "like" ?
+                <div className="notification-container">
+                  <Link className="me-3 notif-by" to={`/user/${notif.likedBy}`}>
+                    <img src={notif.likedByPhoto ? notif.likedByPhoto : '/blank-profile.jpg'} alt="liked" />
+                    <strong>{notif.likedBy}</strong>
+                  </Link>{' '}
+                  <Link onClick={() => handleOpened(notif)} className="post-link-notif" to={`/post/${notif.postLiked}`}>
+                    <label>Liked your post</label>
+                    <img src={notif.postLikedPhoto} alt="" />
+                  </Link>
+                </div>
+                  :
+                <div className="notification-container">
+                  <Link onClick={() => handleOpened(notif)} className="me-3 notif-by follow-notif-link" to={`/user/${notif.followedBy}`}>
+                    <div>
+                      <img src={notif.likedByPhoto ? notif.likedByPhoto : '/blank-profile.jpg'} alt="liked" />
+                      <strong>{notif.followedBy}</strong>
+                    </div>
+                    <label>Started Following You!</label>
+                  </Link>
+                </div>
+                }
+                
               </li>
             ))
           ) : (
