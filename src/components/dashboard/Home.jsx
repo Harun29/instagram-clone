6b3d6a828e7ba3b23529b77f2818844a4569fcae
Harun29 <ibrahimovic.harun01@gmail.com>
2023-12-06 21:07:29 +0100

    styled like

--- src/components/dashboard/Home.jsx
+++ src/components/dashboard/Home.jsx
@@ -198,7 +198,7 @@ const Home = () => {
 
             <div className="interactions">
               <div>
-                <div onClick={() => handleLike(post.id, post.photo, post.userId, posts.indexOf(post))}>
+                <div key={posts.indexOf(post)} onClick={() => handleLike(post.id, post.photo, post.userId, posts.indexOf(post))}>
                   <svg id={post.id} xmlns="http://www.w3.org/2000/svg" class={`icon icon-tabler icon-tabler-heart icon-tabler-heart ${post.likedBy.includes(userViewing.email) ? "active" : ""}`} width="30" height="30" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill={post.likedBy.includes(userViewing.email) ? "red" : "none"} stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                     <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
