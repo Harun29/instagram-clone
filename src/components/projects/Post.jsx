4504a86e022e0ceb07d4e7011274aa1cc750575a
Harun29 <ibrahimovic.harun01@gmail.com>
2023-12-29 01:30:45 +0100

    like changes on click in post

--- src/components/projects/Post.jsx
+++ src/components/projects/Post.jsx
@@ -269,7 +269,7 @@ const Post = ({ param, postRef }) => {
           <div className="interactions in-post">
             <div>
               <div onClick={handleLike}>
-                <svg id={post.id} xmlns="http://www.w3.org/2000/svg" class={`icon icon-tabler icon-tabler-heart icon-tabler-heart ${post.likedby.includes(currentUser.email) ? "active" : ""}`} width="30" height="30" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill={post.likedby.includes(userViewing.email) ? "red" : "none"} stroke-linecap="round" stroke-linejoin="round">
+                <svg id={post.id} xmlns="http://www.w3.org/2000/svg" class={`icon icon-tabler icon-tabler-heart icon-tabler-heart ${liked ? "active" : ""}`} width="30" height="30" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill={liked ? "red" : "none"} stroke-linecap="round" stroke-linejoin="round">
                   <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                   <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
                 </svg>
