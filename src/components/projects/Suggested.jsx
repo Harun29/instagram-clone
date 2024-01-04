9fc14465a7a82d1bfc12f9dff07c12311b3e5dc9
Harun29 <ibrahimovic.harun01@gmail.com>
2024-01-04 21:19:39 +0100

    loading first 5 people you arent following

--- src/components/projects/Suggested.jsx
+++ src/components/projects/Suggested.jsx
@@ -1,41 +1,66 @@
 import { useEffect, useState } from "react";
-import { db } from "../../config/firebase";
+import { db, storage } from "../../config/firebase";
 import { getDocs, collection, limit, query, where } from "firebase/firestore";
 import { Link } from "react-router-dom";
 import { getDownloadURL } from "firebase/storage";
+import { useAuth } from "../../context/AuthContext";
+import { ref } from "firebase/storage";
 
 const Suggested = () => {
   const [users, setUsers] = useState([]);
+  const {currentUser} = useAuth();
+  const {getUserByEmail} = useAuth();
+  const [userFollowing, setUserFollowing] = useState();
+
+  useEffect(() => {
+    const fetchUserFollowing = async() => {
+      const user = await getUserByEmail(currentUser.email)
+      const userFollowingList = user.following
+      setUserFollowing(userFollowingList);
+    }
+    try{
+      currentUser && fetchUserFollowing(currentUser.email)
+    }catch(err){
+      console.error(err)
+    }
+  }, [currentUser])
+
 
   useEffect(() => {
     const fetchUsers = async () => {
-      const usersRef = query(collection(db, "users"), limit(5));
+      const usersRef = query(collection(db, "users"),where("userName", "not-in", userFollowing), limit(5));
       const users = await getDocs(usersRef);
-      users.forEach((user) => {
-        setUsers((prevUsers) => [...prevUsers, user.data()]);
+      users.forEach(async(user) => {
+        const object = user.data();
+        let userPhoto = 'blank-profile.jpg'
+        if(user.data().pphoto){
+          userPhoto = await getDownloadURL(ref(storage, `profile_pictures/${user.data().pphoto}`))
+        }
+        object.pphoto = userPhoto
+        setUsers((prevUsers) => [...prevUsers, object]);
       });
     };
     try {
-      fetchUsers();
+      userFollowing && fetchUsers();
     } catch (err) {
       console.error(err);
     }
-  }, []);
+  }, [userFollowing]);
 
   useEffect(() => {
     users && console.log(users);
   }, [users]);
 
   return (
-    <div>
-      {users.map((user) => {
-        <div>
+    <div className="suggested-people">
+      {users && users.map((user, index) => {
+        <div key={index} className="suggestion">
           <Link to={`/user/${user.userName}`}>
-            <img src="user.pphoto" alt="" />
+            <img src={user.pphoto} alt="" />
             <span>{user.userName}</span>
           </Link>
           <button>follow</button>
-        </div>;
+        </div>
       })}
     </div>
   );
